0be585a515d077bfec88bd7541dd78532e0f0940
Anna Kodola <anna.kodola@nure.ua>
2024-06-17 01:24:44 +0300

    Update CreateClinic.js

--- apzkr-pzpi-21-7-kodola-anna/Task3-WebClient/src/components/CreateClinic/CreateClinic.js
+++ apzkr-pzpi-21-7-kodola-anna/Task3-WebClient/src/components/CreateClinic/CreateClinic.js
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
-// import './CreateClinic.css';
 
 const CreateClinic = () => {
     const [formData, setFormData] = useState({
