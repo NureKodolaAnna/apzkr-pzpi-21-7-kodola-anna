9aad30680834cfcf2828c7898655685847e150bc
Anna Kodola <anna.kodola@nure.ua>
2024-06-17 01:18:03 +0300

    Update MedicalRecord.js

--- apzkr-pzpi-21-7-kodola-anna/Task1-Server/models/MedicalRecord.js
+++ apzkr-pzpi-21-7-kodola-anna/Task1-Server/models/MedicalRecord.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const medicalRecordSchema = new mongoose.Schema({
     patient_id: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
     doctor_id: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-    clinic_id: { type: mongoose.Schema.Types.ObjectId, ref: 'Clinic', required: true },
+    clinic_id: { type: mongoose.Schema.Types.ObjectId, ref: 'Clinic'},
     date: { type: String, required: true},
     time: { type: String, required: true},
     description: { type: String, required: true },
